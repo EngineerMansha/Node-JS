2e9b9bffc804858b8705e4fd3a914d0560e4cf1c
EngineerMansha <manshauaf@gmail.com>
2022-03-20 15:39:51 +0500

    Learn about array filter

---
+++ Day-1/index.js
@@ -0,0 +1,18 @@
+//let app= require('./app')
+
+
+// var a = 5;
+// var b = 6;
+// let c = a * b;
+// for (i = 0; i < 10; i++)
+// {
+    
+// }
+//console.log(app.z());
+var arr = [1, 3, 4, 5, 6, 7, 8,5,5,5,5,5,5,];
+let res = arr.filter((item) => {
+    
+        return item
+})
+console.log(res);
+
